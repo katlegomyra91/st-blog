e048c5308627319365e71291116a52e51ca8eacc
katlegomyra91 <katlego@myramade.co.za>
2020-05-22 14:34:47 +0200

    change view post view

--- blog-api/resources/views/pages/view_post.blade.php
+++ blog-api/resources/views/pages/view_post.blade.php
@@ -4,6 +4,7 @@
     <div class="jumbotron text-center">
         <h1>{{ $post->title }}</h1>
         <p>written: {{ $post->created_at }}</p>
+        <p><a class="btn btn-primary btn-lg mr-2" href="{{ url('/edit_post/'.$post->id) }}" role="button">Edit Post</a><a class="btn btn-danger btn-lg" href="{{ url('/delete_post/'.$post->id) }}" role="button">Delete Post</a></p>
     </div>
 
     <section id="blog-post">
